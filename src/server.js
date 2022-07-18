45156bce12e36a454b922d75633c8de866c4baac
d9c <david142x@gmail.com>
2022-07-18 08:48:17 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createServer } from "http";
-import cors from "cors";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
