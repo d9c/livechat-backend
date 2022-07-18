f259888867eeff64b3088486d2cd0f37fabad117
d9c <david142x@gmail.com>
2022-07-18 09:01:38 -0300

    Revert last commit

--- src/server.js
+++ src/server.js
@@ -11,10 +11,7 @@ app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "https://d9c.github.io/livechat-frontend",
-      "http://localhost:3000",
-    ],
+    origin: ["https://d9c.github.io", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
